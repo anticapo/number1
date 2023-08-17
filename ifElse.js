d5a51d30313e014a57c3fa8e4d1f8125485f7aa9
anticapo <anticapodreaming@gmail.com>
2023-08-17 15:11:14 +0200

    print out variable name

--- ifElse.js
+++ ifElse.js
@@ -1,7 +1,9 @@
 let variableName = 5
 
-if (variableName == 1)
+if (variableName == 1) {
     console.log ("hey it is 1")
-else 
+} else {
     console.log ("hey this is not 1")
+}
 
+console.log (`The variable contains ${variableName}.`)
